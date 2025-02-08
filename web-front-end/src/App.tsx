3c7313e69e681ebc92b218cac407ed14df1b8c35
Faiza <berrichif0717@gmail.com>
2025-02-08 23:16:42 +0100

    LMD-5 :initialize project with react and vite + config of tailwind css

--- web-front-end/src/App.tsx
+++ web-front-end/src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1 className="text-rose-700">This is text with tailwindcss style</h1>
+    </>
+  )
 }
 
-export default App;
+export default App
