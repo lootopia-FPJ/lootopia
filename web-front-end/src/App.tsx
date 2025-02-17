315ea1f68e21b4e0d00fcbbad7d31ad7524dd748
Faiza <berrichif0717@gmail.com>
2025-02-17 17:24:58 +0100

    add shad cdn installation and doc to start projet fo web front end

--- web-front-end/src/App.tsx
+++ web-front-end/src/App.tsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from '@/components/ui/button'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1 className="text-rose-700">This is text with tailwindcss style</h1>
-    </>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-gray-900 mb-4">Test ShadCN/UI avec Tailwind v4</h1>
+      <Button variant="secondary">Clique-moi</Button>
+    </div>
   )
 }
 
