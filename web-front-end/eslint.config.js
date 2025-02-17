af2bd4e0bffb29d2846a58c883d931e810576ec4
Faiza <berrichif0717@gmail.com>
2025-02-17 16:20:45 +0100

    LMD-5: add new rules for eslin

--- web-front-end/eslint.config.js
+++ web-front-end/eslint.config.js
@@ -6,37 +6,43 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import prettier from 'eslint-plugin-prettier'
 
-
 export default [
   {
-    ignores: ['dist', 'node_modules','eslint.config.js'],
+    ignores: ['dist', 'node_modules', 'eslint.config.js'],
   },
   {
-    files: ['**/*.{ts,tsx,js,jsx}'], 
+    files: ['**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      sourceType: 'module', 
-      parser: tseslint.parser, 
+      sourceType: 'module',
+      parser: tseslint.parser,
       parserOptions: {
         ecmaFeatures: {
-          jsx: true, 
+          jsx: true,
         },
       },
       globals: globals.browser,
     },
     plugins: {
-      react, 
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      prettier, 
+      prettier,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      'prettier/prettier': 'error', 
+      'prettier/prettier': 'error',
       'react/jsx-uses-react': 'off',
-      'react/react-in-jsx-scope': 'off', 
-      'no-console': ['error', { allow: ['warn', 'error'] }], 
+      'react/react-in-jsx-scope': 'off',
+      'no-console': ['error', { allow: ['warn', 'error'] }],
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'no-unused-imports': 'error',
+      'no-var': 'error',
+      complexity: ['error', { max: 5 }],
+      'max-lines-per-function': ['error', { max: 30, skipComments: true }],
+      'space-infix-ops': 'error',
+      'space-before-blocks': 'error',
     },
   },
 ]
