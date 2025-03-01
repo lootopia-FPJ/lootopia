994763d851015d9a011394be3bf6b06222289f1a
Prajay-Chaudhary <prajaychaudhary5678@gmail.com>
2025-03-01 14:40:48 +0100

    refactor: format code

--- mobile-app-front-end/Lootopia/metro.config.js
+++ mobile-app-front-end/Lootopia/metro.config.js
@@ -1,5 +1,5 @@
-const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
-const { withNativeWind } = require("nativewind/metro");
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { withNativeWind } = require('nativewind/metro');
 
 /**
  * Metro configuration
@@ -11,4 +11,4 @@ const config = mergeConfig(getDefaultConfig(__dirname), {
   /* your config */
 });
 
-module.exports = withNativeWind(config, { input: "./global.css" });
+module.exports = withNativeWind(config, { input: './global.css' });
