13f2a92bb6377e8e61a8096b4a10b99de9bd0c25
Faiza <berrichif0717@gmail.com>
2025-03-05 11:49:29 +0100

    LMD-49:delete a comment

--- web-front-end/src/routes/MainRoutes.tsx
+++ web-front-end/src/routes/MainRoutes.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import Navbar from '../components/Navbar'
-import Footer from '../components/Footer' // ✅ Importation du Footer
+import Footer from '../components/Footer'
 import Home from '../pages/Home'
 import About from '../pages/About'
 import Events from '../pages/ Events'
