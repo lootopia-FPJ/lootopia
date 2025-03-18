b3f4331aeb6c7c9b9a3302420a783393bd188970
Prajay-Chaudhary <prajaychaudhary5678@gmail.com>
2025-03-18 09:51:19 +0100

    build: configure app text font family

--- mobile-app-front-end/Lootopia/App.tsx
+++ mobile-app-front-end/Lootopia/App.tsx
@@ -1,12 +1,26 @@
+import {View, Text, StyleSheet} from 'react-native';
 import './global.css';
-import {View, Text} from 'react-native';
 
 const App = () => (
   <View className="flex-1 justify-center items-center bg-gray-200">
     <Text className="text-red-500 text-lg">
       Hello, NativeWind with Tailwind!
     </Text>
+    <View style={styles.main}>
+      <Text style={styles.customText}>Hello, this is the font test text</Text>
+    </View>
   </View>
 );
+
+const styles = StyleSheet.create({
+  main: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  customText: {
+    color: 'red',
+    fontFamily: 'Inter',
+  },
+});
 
 export default App;
