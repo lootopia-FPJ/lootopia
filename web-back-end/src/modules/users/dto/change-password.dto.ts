fdcc50febf678ee1affbe742c9f078209b16a71a
Faiza <berrichif0717@gmail.com>
2025-04-19 16:21:40 +0200

    LMD-59: change message in english

--- web-back-end/src/modules/users/dto/change-password.dto.ts
+++ web-back-end/src/modules/users/dto/change-password.dto.ts
@@ -1,11 +1,13 @@
 import { IsNotEmpty, MinLength, Matches } from 'class-validator'
 
 export class ChangePasswordDto {
-  @IsNotEmpty()
-  @MinLength(8)
-  @Matches(/[A-Z]/, { message: 'Au moins une majuscule' })
-  @Matches(/[a-z]/, { message: 'Au moins une minuscule' })
-  @Matches(/[0-9]/, { message: 'Au moins un chiffre' })
-  @Matches(/[@$!%*?&]/, { message: 'Au moins un caractère spécial' })
+  @IsNotEmpty({ message: 'Password is required' })
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  @Matches(/[A-Z]/, { message: 'Password must contain at least one uppercase letter' })
+  @Matches(/[a-z]/, { message: 'Password must contain at least one lowercase letter' })
+  @Matches(/[0-9]/, { message: 'Password must contain at least one digit' })
+  @Matches(/[@$!%*?&]/, {
+    message: 'Password must contain at least one special character (@$!%*?&)',
+  })
   newPassword!: string
 }
